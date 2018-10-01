ca8455652ebaff262f1d16a766f2b3b1cc406126
Nickolas <nicknesk@gmail.com>
2018-10-01 11:39:21 +0300

    battery icons added

--- src/components/common/BatteryIcon.jsx
+++ src/components/common/BatteryIcon.jsx
@@ -2,9 +2,26 @@ import React from "react";
 import PropTypes from "prop-types";
 import Icon from "@material-ui/core/Icon/Icon";
 
+const icons = (level) => {
+    if (level >95) {
+        return <i className="fa fa-battery-full" aria-hidden="true"/>;
+    } else if (level > 50) {
+        return <i className="fa fa-battery-three-quarters" aria-hidden="true"/>;
+    } else if (level > 25) {
+        return <i className="fa fa-battery-half" aria-hidden="true"/>;
+    } else if (level > 10) {
+        return <i className="fa fa-battery-quarter" aria-hidden="true"/>;
+    } else {
+        return <i className="fa fa-battery-empty" aria-hidden="true"/>;
+    }
+};
+
 class BatteryIcon extends React.Component {
     render () {
-        const { battery_level, cssClass } = this.props;
+        const { battery_level: raw_level, cssClass } = this.props;
+        const battery_level = Math.round(raw_level);
+        const icon = icons(battery_level);
+
         return (
             <div className = { cssClass.battery_root }>
                 <div className = { cssClass.label }>
@@ -12,7 +29,8 @@ class BatteryIcon extends React.Component {
                         %
                 </div>
                 <Icon className = { cssClass.icon }>
-                    <i className = "fa fa-battery-three-quarters" aria-hidden = "true" />
+                    { icon }
+                    {/*<i className = "fa fa-battery-three-quarters" aria-hidden = "true" />*/}
                 </Icon>
             </div>
         );
